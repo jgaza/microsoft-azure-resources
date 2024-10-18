139924e08fd8b09b8b11fa520c586177fdaae7ba
Joseph Gazalet <49173224+jgaza@users.noreply.github.com>
2024-10-18 18:54:49 +0200

    docs: include documentation comment for PowerShell script
    Signed-off-by: Joseph Gazalet <49173224+jgaza@users.noreply.github.com>

--- arm-templates/Deploy-ArmTemplate.ps1
+++ arm-templates/Deploy-ArmTemplate.ps1
@@ -1,3 +1,50 @@
+<#
+.SYNOPSIS
+    A script for deploying ARM templates to Azure.
+
+.DESCRIPTION
+    This script enables deploying Azure resources through the Azure Reource Manager (ARM) by using valid template files in either the JSON or Bicep format.
+	It also accepts parameters passed using a paramater file, or inline (or both simultaneously).
+
+	The script uses either the native 'New-AzResourceGroupDeployment' cmdlet (default), or the Azure CLI to initiate the deployment.
+
+.PARAMETER ResourceGroupName
+    The name of the target Resource Group to which the resources described by the ARM template should be deployed.
+
+.PARAMETER TemplateFilePath
+    The relative or absolute path to the '.json' or '.bicep' template file (JSON or Bicep format).
+
+.PARAMETER TemplateParameterFilePath
+    Optional. The relative or absolute path to the '.json' or '.bicepparam' parameter file.
+
+.PARAMETER AzCli
+    Optional switch parameter. If specified, invokes the Azure CLI for parsing the ARM template and initiating the deployment.
+
+.PARAMETER InlineParameter
+    Optional. An ordered dictionary containing additional parameters to be passed to the ARM template and not already specified in a parameter file.
+
+.EXAMPLE
+	$inlineParameters = [ordered] @{
+		AdditionalParam1 = 'value1';
+		AdditionalParam2 = 'value2';
+		SecretParam = Read-Host -AsSecureString
+	}
+
+	& .\Deploy-SandboxUbuntuVM.ps1 `
+		-ResourceGroupName my-rg-name `
+		-TemplateFilePath <path-to-template-file> `
+		-TemplateParameterFilePath <path-to-parameter-file>  `
+		-InlineParameter $inlineParameters `
+		-AzCli
+
+.NOTES
+    This script is designed to work with PowerShell 7. Compatibility with PowerShell 5.1 is not guaranteed.
+
+.LINK
+	https://learn.microsoft.com/en-us/azure/azure-resource-manager/templates/
+	https://learn.microsoft.com/en-us/azure/azure-resource-manager/templates/deploy-powershell
+    https://learn.microsoft.com/en-us/azure/azure-resource-manager/templates/deploy-cli
+#>
 
 [CmdletBinding()]
 Param(
