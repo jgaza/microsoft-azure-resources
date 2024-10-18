b6a4f5871a9c47d0af869bc1777ee3871476a4c7
Joseph Gazalet <49173224+jgaza@users.noreply.github.com>
2024-10-18 17:55:24 +0200

    feat: enhance PowerShell script for deploying ARM tempaltes and enable handling requests through the Azure CLI
    Signed-off-by: Joseph Gazalet <49173224+jgaza@users.noreply.github.com>

--- arm-templates/Deploy-SandboxUbuntuVM.ps1
+++ arm-templates/Deploy-SandboxUbuntuVM.ps1
@@ -1,13 +1,67 @@
-$resourceGroupName = Read-Host -Prompt 'Please enter the name of the resource group to which the VM should be assigned'
-$vNetName = Read-Host -Prompt 'Please enter the name of the VNet to which the VM should be connected'
-$subNetName = Read-Host -Prompt 'Please enter the name of the subnet to which the VM should be connected'
-$adminPublicKey = Read-Host -Prompt 'Please enter the public key to use for VM authentication' -AsSecureString
 
-New-AzResourceGroupDeployment `
-	-Name SandboxDeployment `
-	-ResourceGroupName $resourceGroupName `
-	-TemplateFile .\vm.json `
-	-TemplateParameterFile .\parameters\vm_sandbox_ubuntu-server_armarch.json `
-	-virtualNetworkName $vNetName `
-	-subnetName $subNetName `
-	-adminPublicKey $adminPublicKey
+[CmdletBinding()]
+Param(
+	$ResourceGroupName,
+	$TemplateFilePath,
+	[Parameter(Mandatory = $false)]
+	$TemplateParameterFilePath,
+	[Parameter(Mandatory = $false)]
+	[switch] $AzCli,
+	[Parameter(Mandatory = $False)]
+	[hashtable] $InlineParameter
+)
+
+$date = Get-Date -Format 'MM-dd-yyyy_HHmm'
+$templateFileName = (Get-ChildItem $TemplateFilePath).Name
+$deploymentName = "$($date)_$($templateFileName)_template-deployment"
+
+Write-Information "Deploying resource to Resource Group '$ResourceGroupName' using template '$templateFileName'"
+
+if ($AzCli) {
+
+	Write-Information 'Using Azure CLI...'
+
+	$azCliCommand = "az deployment group create --name $deploymentName --resource-group $ResourceGroupName --template-file $TemplateFilePath"
+
+	[string[]] $parameterArray = @()
+
+	if	($TemplateParameterFilePath) {
+		$parameterArray += $TemplateParameterFilePath
+	}
+
+	if ($InlineParameter -and 0 -lt $InlineParameter.Count) {
+
+		$InlineParameter.GetEnumerator() | ForEach-Object {
+
+			$parameterValue = ([System.Security.SecureString] -eq $_.Value.GetType()) ? (ConvertFrom-SecureString -AsPlainText -SecureString $_.Value) : $_.Value
+			$parameterArray += "$($_.Key)=$parameterValue"
+
+		}
+	}
+
+	if (0 -lt $parameterArray.Count) {
+		$azCliCommand  += " --parameters $($parameterArray -join ' ' )"
+	}
+
+	Invoke-Expression $azCliCommand
+
+} else {
+
+	Write-Information 'Using PowerShell...'
+
+	$psArguments = [ordered]  @{
+		Name              = $deploymentName;
+		ResourceGroupName = $ResourceGroupName;
+		TemplateFile      = $TemplateFilePath
+	}
+
+	if	($TemplateParameterFilePath) {
+		$psArguments.TemplateParameterFile = $TemplateParameterFilePath
+	}
+
+	$psArguments = ($InlineParameter -and 0 -lt $InlineParameter.Count) ? ($psArguments += $InlineParameter) : $psArguments
+
+	New-AzResourceGroupDeployment @psArguments
+}
+
+Write-Information 'Resource deployment complete.'
