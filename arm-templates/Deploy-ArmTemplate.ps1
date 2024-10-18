22072fcfd9f246b36b58712341ae7be3f37029d5
Joseph Gazalet <49173224+jgaza@users.noreply.github.com>
2024-10-18 19:48:26 +0200

    fix: properly handle parameter values containing spaces
    Signed-off-by: Joseph Gazalet <49173224+jgaza@users.noreply.github.com>

--- arm-templates/Deploy-ArmTemplate.ps1
+++ arm-templates/Deploy-ArmTemplate.ps1
@@ -1,27 +1,27 @@
 <#
 .SYNOPSIS
-    A script for deploying ARM templates to Azure.
+	A script for deploying ARM templates to Azure.
 
 .DESCRIPTION
-    This script enables deploying Azure resources through the Azure Reource Manager (ARM) by using valid template files in either the JSON or Bicep format.
+	This script enables deploying Azure resources through the Azure Reource Manager (ARM) by using valid template files in either the JSON or Bicep format.
 	It also accepts parameters passed using a paramater file, or inline (or both simultaneously).
 
 	The script uses either the native 'New-AzResourceGroupDeployment' cmdlet (default), or the Azure CLI to initiate the deployment.
 
 .PARAMETER ResourceGroupName
-    The name of the target Resource Group to which the resources described by the ARM template should be deployed.
+	The name of the target Resource Group to which the resources described by the ARM template should be deployed.
 
 .PARAMETER TemplateFilePath
-    The relative or absolute path to the '.json' or '.bicep' template file (JSON or Bicep format).
+	The relative or absolute path to the '.json' or '.bicep' template file (JSON or Bicep format).
 
 .PARAMETER TemplateParameterFilePath
-    Optional. The relative or absolute path to the '.json' or '.bicepparam' parameter file.
+	Optional. The relative or absolute path to the '.json' or '.bicepparam' parameter file.
 
 .PARAMETER AzCli
-    Optional switch parameter. If specified, invokes the Azure CLI for parsing the ARM template and initiating the deployment.
+	Optional switch parameter. If specified, invokes the Azure CLI for parsing the ARM template and initiating the deployment.
 
 .PARAMETER InlineParameter
-    Optional. An ordered dictionary containing additional parameters to be passed to the ARM template and not already specified in a parameter file.
+	Optional. An ordered dictionary containing additional parameters to be passed to the ARM template and not already specified in a parameter file.
 
 .EXAMPLE
 	$inlineParameters = [ordered] @{
@@ -38,12 +38,12 @@
 		-AzCli
 
 .NOTES
-    This script is designed to work with PowerShell 7. Compatibility with PowerShell 5.1 is not guaranteed.
+	This script is designed to work with PowerShell 7. Compatibility with PowerShell 5.1 is not guaranteed.
 
 .LINK
 	https://learn.microsoft.com/en-us/azure/azure-resource-manager/templates/
 	https://learn.microsoft.com/en-us/azure/azure-resource-manager/templates/deploy-powershell
-    https://learn.microsoft.com/en-us/azure/azure-resource-manager/templates/deploy-cli
+	https://learn.microsoft.com/en-us/azure/azure-resource-manager/templates/deploy-cli
 #>
 
 [CmdletBinding()]
@@ -81,13 +81,13 @@ if ($AzCli) {
 		$InlineParameter.GetEnumerator() | ForEach-Object {
 
 			$parameterValue = ([System.Security.SecureString] -eq $_.Value.GetType()) ? (ConvertFrom-SecureString -AsPlainText -SecureString $_.Value) : $_.Value
-			$parameterArray += "$($_.Key)=$parameterValue"
+			$parameterArray += "$($_.Key)='$parameterValue'"
 
 		}
 	}
 
 	if (0 -lt $parameterArray.Count) {
-		$azCliCommand  += " --parameters $($parameterArray -join ' ' )"
+		$azCliCommand += " --parameters $($parameterArray -join ' ' )"
 	}
 
 	Invoke-Expression $azCliCommand
